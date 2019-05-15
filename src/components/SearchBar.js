256202f1da089f23203e8d3e0037c1f4eeb65185
ping58972 <ping58972@gmail.com>
2019-05-15 16:23:16 -0500

    Try to fetch data from youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,8 +8,7 @@ class SearchBar extends React.Component {
     }
     onFormSumit = (e) => {
         e.preventDefault();
-        //ToDo: Make sure we call callback 
-        //from parent component.
+        this.props.onFormSubmit(this.state.term);
     }
     render() {
         return (
