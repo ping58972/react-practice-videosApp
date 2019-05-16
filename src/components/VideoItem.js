da9160ceaaf000bc80927c41cdf18d0bfcf52aee
ping58972 <ping58972@gmail.com>
2019-05-16 00:09:14 -0500

    Finished Video App

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,12 +2,13 @@ import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({video, onVideoSelect }) => {
+    const title = video.snippet.title
   return (
     <div onClick={() => onVideoSelect(video)} className="item video-item">
-        <img className="ui image" src={video.snippet.thumbnails.medium.url} alt=""/>
+        <img alt={title} className="ui image" src={video.snippet.thumbnails.medium.url} />
         <div className="content">
           <div className="header">
-          {video.snippet.title}
+          {title}
           </div>
         </div>
       
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,8 +2,10 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({videos, onVideoSelect }) => {
+    
     const renderList = videos.map(video => {
-        return <VideoItem onVideoSelect = {onVideoSelect} video = {video}/>;
+        return( 
+        <VideoItem key={video.id.videoId}  onVideoSelect = {onVideoSelect} video = {video}/>);
     });
     return (
         <div className = "ui relaxed divided list">
